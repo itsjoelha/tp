c7008dc4abb10d161a5c5fa09ad57605fa0e9b4c
Jonathan2745 <e1157198@u.nus.edu>
2025-03-20 04:15:14 +0800

    Gradle Checkes #14

--- src/main/java/seedu/duke/data/Mod.java
+++ src/main/java/seedu/duke/data/Mod.java
@@ -17,7 +17,7 @@ public class Mod {
 
     public Mod(String code) {
         // Use Paths.get() for platform-independent file paths
-        String filePath = "./data/mod_data.txt";
+        String filePath = "data/mod_data.txt";
         Data datafile = new Data(filePath);
         String[] parts = datafile.searchMod(code);
         this.name = parts[1];
