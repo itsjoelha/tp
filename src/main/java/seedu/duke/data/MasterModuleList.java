47029e7a8d5ad4a2b2b30553cbf0e79bb67a6fbe
Jonathan2745 <e1157198@u.nus.edu>
2025-03-20 20:53:50 +0800

    Gradle #23

--- src/main/java/seedu/duke/data/MasterModuleList.java
+++ src/main/java/seedu/duke/data/MasterModuleList.java
@@ -16,17 +16,20 @@ public class MasterModuleList {
     }
 
     private static void loadModulesFromFile() {
-        try (InputStream inputStream = MasterModuleList.class.getClassLoader().getResourceAsStream("moduledata.txt")) {
+        try (InputStream inputStream = MasterModuleList.class.getClassLoader()
+                .getResourceAsStream("moduledata.txt")) {
             if (inputStream == null) {
                 System.err.println("File not found: moduledata.txt");
                 return;
             }
-            try (BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream, StandardCharsets.UTF_8))) {
+            try (BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream,
+                    StandardCharsets.UTF_8))) {
                 String line;
                 while ((line = reader.readLine()) != null) {
                     String[] parts = line.split("\\|");
                     if (parts.length == 4) {
-                        modules.add(new Mod(parts[1].trim(), parts[3].trim(), Integer.parseInt(parts[2].trim()), parts[0].trim()));
+                        modules.add(new Mod(parts[1].trim(), parts[3].trim(),
+                                Integer.parseInt(parts[2].trim()), parts[0].trim()));
                     } else {
                         System.err.println("Invalid line format: " + line);
                     }
