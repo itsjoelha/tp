a5c962043fd975e3560eda75668ce35b84778093
Piraveen Alexander <e1122425@u.nus.edu>
2025-04-01 14:10:59 +0800

    fix issue with /workload accessing module data from wrong source

--- src/main/java/seedu/duke/command/Workload.java
+++ src/main/java/seedu/duke/command/Workload.java
@@ -1,13 +1,13 @@
 package seedu.duke.command;
 
-import static seedu.duke.Duke.moduleList;
+import static seedu.duke.Duke.currentUser;
 import seedu.duke.data.Mod;
 
 public class Workload implements Command {
     public Workload() {}
 
     public void execute() {
-        if (moduleList.isEmpty()) {
+        if (currentUser.getAllModules().isEmpty()) {
             System.out.println("No modules in List");
             return;
         }
@@ -16,7 +16,7 @@ public class Workload implements Command {
         System.out.println("| Code      | MCs | Lecture Hrs | Tut Hrs | Lab Hrs | Proj Hrs | Prep Hrs |");
         System.out.println("+-----------+-----+-------------+---------+---------+----------+----------+");
 
-        for (Mod m : moduleList) {
+        for (Mod m : currentUser.getAllModules()) {
             System.out.printf("| %-9s | %-3d | %-11.1f | %-7.1f | %-7.1f | %-8.1f | %-8.1f |\n",
                     m.getCode(), m.getNumMC(), m.getLectureHours(), m.getTutHours(),
                     m.getLabHours(), m.getProjHours(), m.getPrepHours());
