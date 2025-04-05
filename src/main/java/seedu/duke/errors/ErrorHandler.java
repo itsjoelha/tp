5e18d1b5a7eadba30813eeac332253e827894ba7
XMTAN\xmtan <xmtan9999@gmail.com>
2025-04-06 01:11:11 +0800

    fix gradle indentation

--- src/main/java/seedu/duke/errors/ErrorHandler.java
+++ src/main/java/seedu/duke/errors/ErrorHandler.java
@@ -39,25 +39,25 @@ public class ErrorHandler {
             break;
         }
         Ui.printUserInputError(command);
-        }
-
-        public static void integerInputError (String word, String command){
-            if (word.equals("semester")) {
-                logger.warning("Invalid semester format in " + command + " command.");
-                System.out.println("Error: Semester must be a number between 1 and 8.");
-            } else if (word.equals("creditNum")) {
-                logger.warning("Invalid credit number format in " + command + " command.");
-                System.out.println("Error: Credit number must be a positive integer.");
-            }
-
-        }
-
-        public static void excessInputError (String command){
-            logger.warning("Additional inputs for " + command + " command.");
-            Ui.printExcessInputError(command);
-        }
-
-        public static void handleModuleDoesNotExist () {
-            // System.err.println("[ERROR] Module not found");
-        }
     }
+
+    public static void integerInputError(String word, String command) {
+        if (word.equals("semester")) {
+            logger.warning("Invalid semester format in " + command + " command.");
+            System.out.println("Error: Semester must be a number between 1 and 8.");
+        } else if (word.equals("creditNum")) {
+            logger.warning("Invalid credit number format in " + command + " command.");
+            System.out.println("Error: Credit number must be a positive integer.");
+        }
+
+    }
+
+    public static void excessInputError(String command) {
+        logger.warning("Additional inputs for " + command + " command.");
+        Ui.printExcessInputError(command);
+    }
+
+    public static void handleModuleDoesNotExist() {
+        // System.err.println("[ERROR] Module not found");
+    }
+}
