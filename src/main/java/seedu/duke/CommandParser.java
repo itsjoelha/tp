5475c208245a5acdf0b887baf567227d3bf9552e
Jonathan2745 <e1157198@u.nus.edu>
2025-04-05 12:11:15 +0800

    typo

--- src/main/java/seedu/duke/CommandParser.java
+++ src/main/java/seedu/duke/CommandParser.java
@@ -182,7 +182,7 @@ public class CommandParser {
         case "/schedule":
             if (words.length > 1) {
                 logger.warning("Specialisation command doesn't accept additional arguments.");
-                System.out.println("Error: The '/spec' command doesn't accept any arguments.");
+                System.out.println("Error: The '/schedule' command doesn't accept any arguments.");
             } else {
                 logger.info("Executing RecommendedSchedule command.");
                 cmdObject = new RecommendedSchedule();
